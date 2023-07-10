3e0c15ce1c40b13f66c093a777b0d215c697ec47
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-07-10 14:06:43 +0530

    Using the variable for the function call

--- src/start.c
+++ src/start.c
@@ -1,6 +1,10 @@
 extern int write_call(int fd, char *string, int length);
 extern void exit_call(int rs);
 void _start(void) {
-  write_call(1, "Hello, World!\n", 15);
+  char *hello = "Hello, World\n";
+  int return_val = write_call(1, hello, 15);
+  if (return_val == -1) {
+    exit_call(1);
+  }
   exit_call(0);
 }
